9ce7f10b0a39c70952906c25e352779d05aafd3b
Victor Espaillat <vicander_99@hotmail.com>
2020-07-20 09:57:49 -0400

    Init

---
+++ index.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const config = require('config');
+const dbURI = config.get('dbURI');
+
+const projectSchema = new mongoose.Schema({
+  title: String,
+  href: String,
+  tags: [String],
+  length: Number,
+  data: Buffer
+});
+
+const Project = mongoose.model('Project', projectSchema, 'projects');
+
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log(`Connected to ${dbURI}...`))
+  .catch(err => console.log('Could not connect to MongoDB...', err));
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/api/projects', async (req, res) => {
+  const project = await Project.find({}).select('-data');
+  res.send(project);
+});
+
+const port = process.env.PORT || config.get('port');
+app.listen(port, () => console.log(`Listening on port ${port}...`));
